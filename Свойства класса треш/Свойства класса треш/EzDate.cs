1b3757576b18476e569e6e5c41fab1a38c25828a
AkioSe7en <114975962+AkioSe7en@users.noreply.github.com>
2022-11-26 02:58:04 +0700

    Add files via upload

---
+++ Свойства класса треш/Свойства класса треш/EzDate.cs
@@ -0,0 +1,41 @@
+﻿namespace Свойства_класса_треш;
+
+class EzDate
+{
+    private DateTime date;
+    public DateTime Date 
+   {
+       get => date;
+       set
+       {
+           date = value;
+       }
+   }
+
+    public bool Viso
+    {
+        get
+        {
+            return DateTime.IsLeapYear(date.Year);
+        }
+    }
+
+   public EzDate() => this.date = new DateTime(2009,01,01); //Конструктор по умолчанию
+   public EzDate(DateTime date) => this.date = date; //Кастом конструктор
+   
+   public void PresentDay() => WriteLine($"Нынешняя дата: {date.Day:00}.{date.Month:00}.{date.Year:0000}"); // текущий день
+
+   public void PreviousDay() //предыдущий день
+   {
+       var prev = date.AddDays(-1);
+       WriteLine($"Предыдущий день: {prev.ToShortDateString()}");
+   } 
+   public void NextDay() // Следующий день
+   {
+       var next = date.AddDays(1);
+       WriteLine($"Следующий день: {next.ToShortDateString()}");
+   }
+   public void EndMonth() => WriteLine($"До конца месяца осталось {DateTime.DaysInMonth(date.Year, date.Month)-date.Day} дней");
+   
+
+}
---
+++ Свойства класса треш/Свойства класса треш/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using Свойства_класса_треш;
+
+WriteLine("Программа для работы с датами.");
+Nachalo:
+WriteLine("Введите 1, если хотите ввести дату вручную");
+WriteLine("Введите 2, если хотите получить дату по стандарту");
+WriteLine("Введите 3, если хотите получить сегодняшний день");
+string choise = ReadLine();
+EzDate Date = new EzDate();
+
+switch (choise)
+{
+    case "1":
+        Write("Введите год в формате Day.Month.Year   ");
+        var ReadDate = ReadLine();
+        while (DateTime.TryParse(ReadDate, out var z)==false)
+            {
+                Write("Неверный формат даты. Попробуйте еще раз: ");
+                ReadDate = ReadLine();
+            }
+        var dateSplit= ReadDate.Split(".");
+        Date = new EzDate(new DateTime(int.Parse(dateSplit[2]), int.Parse(dateSplit[1]), int.Parse(dateSplit[0])));
+        break;
+    
+    case "2":
+        Date = new EzDate();
+        break;
+    case "3":
+        Date.Date = DateTime.Now;
+        break;
+    default:
+        WriteLine("Вы ввели неверную дату");
+        WriteLine();
+        goto Nachalo;
+        break;
+        }
+
+WriteLine();
+Date.PreviousDay();
+Date.PresentDay();
+Date.NextDay();
+Date.EndMonth();
+WriteLine(Date.Viso == true ? "Год является високосным." : "Год не является високосным");
